fb81aba12b3a50048ef482db5211131521975724
João Gomes <joaoantoniog96@gmail.com>
2021-11-08 22:55:32 -0300

    Arquivos básicos para projeto Servlet utilizando intelliJ

---
+++ src/main/java/br/com/servlet/PrimeiraServlet.java
@@ -0,0 +1,19 @@
+package br.com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/primeira")
+public class PrimeiraServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body><h1>Primeir Servlet!</h1></body></html>");
+    }
+}
